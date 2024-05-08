e4271a7d006de3aeeb21eba4e74b6b77d6908e34
AjayFrancisTechversant <ajay.francis@techversantinfotech.com>
2024-05-08 17:59:58 +0530

    universal card component made,but not called in home page

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -45,23 +45,3 @@ const Card = ({ index, item, handleDelete, Component }) => {
 
 export default Card
 
-{/* <View style={screenStyles.itemContainer}>
-                        
-<View style={screenStyles.imageContainer} >
-    <Image 
-        style={screenStyles.image}
-        source={{ uri: item.uri }}
-    />
-</View>
-<View style={screenStyles.titleandDescContainer}>
-    <Text style={screenStyles.itemTitle} >{item.title}</Text>
-    <Text >{item.desc}</Text>
-    </View>
-<View style={screenStyles.deleteContainer}>
-    <TouchableOpacity onPress={() => handleDelete(index)} >
-        <View style={screenStyles.deleteButton}>
-            <Text ><Icon name='delete' size={30} color={'red'} /></Text>
-        </View>
-    </TouchableOpacity>
-</View>
-</View> */}
