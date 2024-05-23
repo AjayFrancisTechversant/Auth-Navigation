2ca6b2dba0283e878acefad69d8659f6ba1388ae
AjayFrancisTechversant <ajay.francis@techversantinfotech.com>
2024-05-23 15:21:05 +0530

    sliderButton roughly completed

--- src/Components/SlidingButton/SlidingButton.js
+++ src/Components/SlidingButton/SlidingButton.js
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { Alert, Text, View } from 'react-native'
 import React from 'react'
 import Animated, {
     useAnimatedStyle,
@@ -15,49 +15,56 @@ import styles from './Style';
 
 function clamp(val, min, max) {
     return Math.min(Math.max(val, min), max);
-  }
+}
 
 const SlidingButton = () => {
     const translationX = useSharedValue(0);
     const prevTranslationX = useSharedValue(0);
     //height,width,sliderWidth should be passed as props 
-    const height=70
-    const width=350
-    const sliderWidth=70
-    
-    const screenStyles = styles(height,width,sliderWidth);
+    const height = 70
+    const width = 350
+    const sliderWidth = 70
+
+    const screenStyles = styles(height, width, sliderWidth);
     const animatedStyles = useAnimatedStyle(() => ({
         transform: [
-          { translateX: translationX.value },
+            { translateX: translationX.value },
         ],
-      }));
+    }));
 
     const pan = Gesture.Pan()
         .minDistance(1)
-        .onStart(()=>
-            prevTranslationX.value=translationX.value
+        .onStart(() =>
+            prevTranslationX.value = translationX.value
         )
-       
+
         .onUpdate((event) => {
-            const maxTranslateX = width-sliderWidth-5
-            const  minTranslateX = 0
+            const maxTranslateX = width - sliderWidth - 5
+            const minTranslateX = 0
 
             translationX.value = withSpring(clamp(
                 prevTranslationX.value + event.translationX,
                 minTranslateX,
                 maxTranslateX
-              ))
-          })
-          
-        .activateAfterLongPress(5000)
+            ))
+            if (translationX.value > width * 0.65) {
+                Alert.alert('hi')
+            }
+            console.log(translationX.value / width);
+        })
+        .onEnd(() => {
+            if (translationX.value < width * 0.65) {
+                withSpring(translationX.value=0)
+            }
+        })
         .runOnJS(true)
-        
+
 
     return (
         <View style={screenStyles.container}>
             <GestureHandlerRootView>
                 <GestureDetector gesture={pan}>
-                    <Animated.View style={[animatedStyles,screenStyles.slider]}>
+                    <Animated.View style={[animatedStyles, screenStyles.slider]}>
                         <Text>Slider</Text>
                     </Animated.View>
                 </GestureDetector>
