098b81af33000ee1b493b25eb37a6b45a050d506
AjayFrancisTechversant <ajay.francis@techversantinfotech.com>
2024-06-18 13:20:47 +0530

    firebase cloud storage not integrated

--- src/Stacks/HomeTabStack.js
+++ src/Stacks/HomeTabStack.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import Listing from '../screens/Listing/Listing'
 import Me from '../screens/Me/Me'
 import Entypo from 'react-native-vector-icons/Entypo';
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
-import Feather from 'react-native-vector-icons/Feather'
-import FontAwesome6 from 'react-native-vector-icons/FontAwesome6'
+
 import HomeDrawerStack from './HomeDrawerStack'
-import Notes from '../screens/Notes/Notes';
+import ImageUploader from '../screens/ImageUploader/ImageUploader';
 
 const HomeTabStack = () => {
     const HomeTabStack = createBottomTabNavigator()
@@ -24,12 +22,19 @@ const HomeTabStack = () => {
 
                 }}
             />
-
+  <HomeTabStack.Screen
+                name='Image Upload'
+                component={ImageUploader}
+                options={{headerShown:false,
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="user" color={color} size={size} />
+                    )
+                }}
+            />
             <HomeTabStack.Screen
                 name='Me'
                 component={Me}
                 options={{
-
                     tabBarIcon: ({ color, size }) => (
                         <FontAwesome name="user" color={color} size={size} />
                     )
