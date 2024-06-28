8988d5f35ba7ae119aa55880ad6487e6363b5a31
AjayFrancisTechversant <ajay.francis@techversantinfotech.com>
2024-06-28 10:05:51 +0530

    .

--- src/Redux/Slices/AddFriendSlice.js
+++ src/Redux/Slices/AddFriendSlice.js
@@ -19,28 +19,3 @@ export const AddFriendSlice = createSlice({
 export const { updateFriends } = AddFriendSlice.actions
 
 export default AddFriendSlice.reducer
-
-// import { createSlice } from '@reduxjs/toolkit'
-// import StaticVariables from '../../Preferences/StaticVariables'
-
-// const initialState = {
-//     addedFriends: StaticVariables.EMPTY_ARRAY
-// }
-
-// export const AddFriendSlice = createSlice({
-//     name: 'AddFriend',
-//     initialState,
-//     reducers: {
-//         addFriend: (state, action) => {
-//             state.addedFriends.push(action.payload)
-//         },
-//         removeFriend: (state, action) => {
-//             state.addedFriends=state.addedFriends.filter(i=>i.id.value!=action.payload.id.value)
-//         },
-//     },
-// })
-
-// // Action creators are generated for each case reducer function
-// export const { addFriend, removeFriend } = AddFriendSlice.actions
-
-// export default AddFriendSlice.reducer
