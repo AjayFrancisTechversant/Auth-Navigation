6178ba319c1cc665dbabdf461b984fdb9beafd22
AjayFrancisTechversant <ajay.francis@techversantinfotech.com>
2024-06-28 09:19:02 +0530

    addfriend logic changed..now more optimized

--- src/Components/AddFriendButton/AddFriendButton.js
+++ src/Components/AddFriendButton/AddFriendButton.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {View, TouchableOpacity} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
@@ -8,17 +8,17 @@ import {addFriend, removeFriend} from '../../Redux/Slices/AddFriendSlice';
 const AddFriendButton = ({item}) => {
   const dispatch = useDispatch();
   const addedFriends = useSelector(state => state.AddFriend.addedFriends);
-  const [friendAdded, setFriendAdded] = useState(
-    addedFriends.some(i => i.id.value == item.id.value),
-  );
+
+  const friendAdded = addedFriends.some(i => i.id.value === item.id.value);
+
   const handleAddFriend = () => {
     if (friendAdded) {
       dispatch(removeFriend(item));
     } else {
       dispatch(addFriend(item));
     }
-    setFriendAdded(!friendAdded);
   };
+
   return (
     <View>
       <TouchableOpacity onPress={handleAddFriend}>
